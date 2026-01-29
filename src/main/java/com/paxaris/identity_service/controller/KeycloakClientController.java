5de6783f73fb6157b0972f3e22ac32ec461c934c
Paxaris Global <product@paxarisglobal.com>
2026-01-29 17:45:58 +0530

    Update KeycloakClientController.java

--- src/main/java/com/paxaris/identity_service/controller/KeycloakClientController.java
+++ src/main/java/com/paxaris/identity_service/controller/KeycloakClientController.java
@@ -391,7 +391,7 @@ public class KeycloakClientController {
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
-    @GetMapping("identity/users/{realm}/")
+    @GetMapping("identity/users/{realm}")
     public ResponseEntity<List<Map<String, Object>>> getAllUsers(@PathVariable String realm) {
         try {
             String masterToken = clientService.getMyRealmToken("admin", "admin123", "admin-cli", "master")
